cbd31f3e8ea3b1f2531ce4126332ad92d6af486a
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-02 22:13:18 +0100

    fixed dir

--- src/lib/plugins/plugins-dir.ts
+++ src/lib/plugins/plugins-dir.ts
@@ -1,10 +1,5 @@
 import path from "node:path";
 import { PLUGINS_DIR } from "../core/constants";
+import { ROOT_DIR } from "../core/root-dir";
 
-export const pluginsDir = path.join(
-  __dirname,
-  "..",
-  "..",
-  "config",
-  PLUGINS_DIR
-);
+export const pluginsDir = path.join(ROOT_DIR, PLUGINS_DIR);
