abdcb618fc238bfb5ef9f0e5526c585c9925fb8e
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-02 22:45:17 +0100

    add doc comments

--- src/lib/plugins/plugin.ts
+++ src/lib/plugins/plugin.ts
@@ -1,6 +1,15 @@
-import { Client, Logger } from 'hass-ts';
+import { Client, Logger } from "hass-ts";
 
 export interface Plugin {
+  /**
+   * Triggered when the plugin is first loaded and on
+   * each reload. Put initialisation logic here
+   */
   initialise: (client: Client, logger: Logger) => Promise<void>;
+
+  /**
+   * Triggered when the plugin is unloaded. This happens just before
+   * every reload. Put cleanup logic here
+   */
   unload?: (logger: Logger) => Promise<void>;
 }
